ec0f58cf6af52f6b07a4f4e549b664d088cbbad4
Stephan Baerwolf <stephan@matrixstorm.com>
2019-11-24 20:05:11 +0000

    further support more AVRs (44pin)
    Signed-off-by: Stephan Baerwolf <stephan@matrixstorm.com>

--- defines.h
+++ defines.h
@@ -20,12 +20,27 @@
 #	define LED_RED		B,3
 #endif
 
-#ifndef PWM_CLOCK1
-#	define PWM_CLOCK1	B,2
-#endif
-#ifndef PWM_CLOCK0
-#	define PWM_CLOCK0	D,4
-#endif
+#if (defined (__AVR_ATmega8__) || defined (__AVR_ATmega8A__) || \
+     defined (__AVR_ATmega88__) || defined (__AVR_ATmega88P__) || defined (__AVR_ATmega88A__) || defined (__AVR_ATmega88PA__) || \
+     defined (__AVR_ATmega168__) || defined (__AVR_ATmega168P__) || defined (__AVR_ATmega168A__) || defined (__AVR_ATmega168PA__) || \
+     defined (__AVR_ATmega328__) || defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328A__) || defined (__AVR_ATmega328PA__))
 
+#   ifndef PWM_CLOCK1
+#   	define PWM_CLOCK1	B,2
+#   endif
+#   ifndef PWM_CLOCK0
+#   	define PWM_CLOCK0	D,4
+#   endif
+
+#else
+
+#   ifndef PWM_CLOCK1
+#	  define PWM_CLOCK1	D,4
+#   endif
+#   ifndef PWM_CLOCK0
+#	  define PWM_CLOCK0	B,0
+#   endif
+
+#endif
 
 #endif
--- source/main.c
+++ source/main.c
@@ -34,7 +34,12 @@
 #if (defined (__AVR_ATmega8__) || defined (__AVR_ATmega8A__) || \
      defined (__AVR_ATmega88__) || defined (__AVR_ATmega88P__) || defined (__AVR_ATmega88A__) || defined (__AVR_ATmega88PA__) || \
      defined (__AVR_ATmega168__) || defined (__AVR_ATmega168P__) || defined (__AVR_ATmega168A__) || defined (__AVR_ATmega168PA__) || \
-     defined (__AVR_ATmega328__) || defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328A__) || defined (__AVR_ATmega328PA__))
+     defined (__AVR_ATmega328__) || defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328A__) || defined (__AVR_ATmega328PA__) || \
+     defined (__AVR_ATmega164__) || defined (__AVR_ATmega164P__) || defined (__AVR_ATmega164A__) || defined (__AVR_ATmega164PA__) || \
+     defined (__AVR_ATmega324__) || defined (__AVR_ATmega324P__) || defined (__AVR_ATmega324A__) || defined (__AVR_ATmega324PA__) || \
+     defined (__AVR_ATmega644__) || defined (__AVR_ATmega644P__) || defined (__AVR_ATmega644A__) || defined (__AVR_ATmega644PA__) || \
+     defined (__AVR_ATmega1284__)|| defined (__AVR_ATmega1284P__)||                                 defined (__AVR_ATmega32__)    || \
+   0)
 void __hwclock_timer_init(void) {
   CFG_INPUT(PWM_CLOCK0);
   CFG_OUTPUT(PWM_CLOCK1);
